b19c6e1d730020af91299e7119a09362ade89ec7
Jeremy Fong <busywater@gmail.com>
2025-12-30 11:08:54 +0000

    Add paginated query and refactor the logging Ack to Gabriel Sroka for the idea of improvement of codes

--- dbs_safeguarding_reminder.py
+++ dbs_safeguarding_reminder.py
@@ -1,35 +1,28 @@
 #!/usr/bin/env python3
 
 '''
-#######################################
-########## Version History ############
-#######################################
-1.0.0 - Initial commit (24/12/2025) - Jeremy Fong
 
 #######################################
 ############ Description ##############
 #######################################
-Purpose
-
 This script integrates with the CHMeetings API to programmatically
 retrieve member records and identify individuals whose DBS and Safeguarding
 certifications have expired or are due to expire within one month.
-
 It generates an Excel report containing the relevant expiry details for review
 by the People Team Coordinator and Safeguarding Coordinator, and automatically
 sends email notifications to affected members.
 
 Deployment
 
-The script can be deployed on GCP or AWS or even GitHub Actions and executed
-on a scheduled basis to ensure
+The script can be deployed on GCP or AWS and executed on a scheduled basis
+(for example, via Cloud Scheduler, EventBridge, or a cron job) to ensure
 continuous monitoring and timely notifications.
 
 '''
 
 ##### IMPORTS ######
 import requests                              # HTTP client for interacting with the CHMeetings REST API
-from datetime import date, timedelta         # Date utilities for certificate expiry calculations
+from datetime import date, datetime, timedelta  # Date utilities for certificate expiry calculations
 import logging                               # Structured logging for operational visibility and debugging
 import os                                    # Access environment variables for configuration and secrets
 import base64                                # Encode email content for Gmail API
@@ -45,16 +38,29 @@ from googleapiclient.discovery import build  # Build Gmail API service client
 
 # Define Script Name
 CODENAME = "DBSSafeguardingReminder"
-__VERSION__ = '1.0.0'
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
+logging.info(
+  'Starting %s',
+  CODENAME
+)
 
 # CHMeetings API Token
 CHMEETINGS_API_TOKEN = os.getenv("CHMEETINGS_API_TOKEN")
 
-# CHMeetings Field ID Enhanced DBS Expiry Date
-DBS_FIELDID = os.getenv("CHMEETINGS_DBS_FIELD_ID", "")
+# SG_FIELD_ID and DBS_FIELD_ID are defined by CHMeetings internally
+# The values can be found in the raw API response of
+# https://api.chmeetings.com/api/v1/people
 
-# CHMeetings Field ID Safeguarding Training Foundation Expiry Date
-SG_FIELDID = os.getenv("CHMEETINGS_SG_FIELD_ID", "")
+# CHMeetings Field ID 887982 - Enhanced DBS Expiry Date
+DBS_FIELD_ID = os.getenv("DBS_FIELD_ID")
+
+# CHMeetings Field ID 887984 - Safeguarding Training Foundation Expiry Date
+SG_FIELD_ID = os.getenv("SG_FIELD_ID")
 
 # Google Client ID
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
@@ -66,14 +72,17 @@ GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 GOOGLE_REFRESH_TOKEN = os.getenv("GOOGLE_REFRESH_TOKEN")
 
 # Google Sender
-GMAIL_SENDER = os.getenv("EMAIL_SENDER", "")
+GMAIL_SENDER = os.getenv("GMAIL_SENDER")
 
+# Google API Scope
 SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 # Email content
-EMAIL_TO = os.getenv("EMAIL_TO", "")
+GMAIL_TO = os.getenv("GMAIL_TO", "")
+
 EMAIL_SUBJECT = "Monthly DBS & Safeguarding Expiry Report"
 EMAIL_BODY = """An Excel report is attached to this email. It contains the latest list of members whose DBS and/or Safeguarding certificates have expired or are due to expire within one month.
+
 Members with expiring certificates have already received an email reminder with instructions on how to renew their certificates. You are advised to contact these members directly to assist them to proceed with the DBS and/or Safeguarding renewal process.
 """
   
@@ -82,7 +91,6 @@ EMAIL_BODY_HTML = """
 <body style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
   <p>An Excel report is attached to this email. It contains the latest list of members whose
   <strong>DBS</strong> and/or <strong>Safeguarding</strong> certificates have expired or are due to expire within one month.</p>
-
   <p>
     Members with expiring certificates have already received an email reminder with instructions
     on how to renew their certificates. You are advised to contact these members directly to assist
@@ -102,7 +110,7 @@ def retrieve_members_info() -> tuple[list, list]:
   """Retrieve a list of members with information of first name, last name, email,
   Enhanced DBS and Safeguarding certificate expiry dates.
   """
-  print(
+  logging.info(
       "[retrieve_members_info] Retrieving members information"
   )
 
@@ -116,103 +124,157 @@ def retrieve_members_info() -> tuple[list, list]:
       "apikey": CHMEETINGS_API_TOKEN,
       "accept": "application/json",
   }
-
-  try:
-    resp = requests.get(endpoint, headers=headers, timeout=10)
-  except requests.RequestException as e:
-    logging.error("[retrieving_members_info] HTTP request failed: %s", e)
-    sys.exit(1)
-
-  if resp.status_code != 200:
-    logging.error("[retrieve_members_info] Unexpected status code %s. Response: %s", resp.status_code, resp.text[:500])
-    sys.exit(1)
   
-  else:
-    print(
-        "[retrieve_members_info] Successfully retrieve data from Chmeeting API"
+  page = 1
+  page_size = 25
+  members = list()
+  
+  # Loop through pages and extend the members list
+  while True:
+    params = {
+      "page": page,
+      "page_size": page_size
+    }
+
+    try:
+      resp = requests.get(
+        endpoint,
+        headers=headers,
+        params=params,
+        timeout=10)
+    except requests.RequestException as e:
+      logging.error(
+        "[retrieving_members_info] HTTP request failed on page: %s: %s",
+        page,
+        e)
+      sys.exit(1)
+
+    if resp.status_code != 200:
+      logging.error(
+        "[retrieve_members_info] Unexpected status code %s on page %s. Response: %s",
+        resp.status_code,
+        page,
+        resp.text[:500])
+      sys.exit(1)
+  
+    # Now we are sure that API query is successful
+    logging.info(
+        "[retrieve_members_info] Successfully retrieve data on page %s from Chmeeting API",
+        page
     )
 
     # Parse into json for further extraction
     data = resp.json()
-    
     results = data.get("data", [])
-    
-    for item in results:
-      
-      # Place all key-value pairs of additional_field into a list
-      additional_fields = item.get("additional_fields", [])
-
-      # Initialised the matched flag
-      matched_dbs = False
-      matched_sg = False     
-      safeguarding_expiry = None
-      dbs_expiry = None
-      
-      for field in additional_fields:
-        if field.get("field_id") == SG_FIELDID:
-          safeguarding_expiry = field.get("value")
-          matched_sg = True
-
-        if field.get("field_id") == DBS_FIELDID:
-          dbs_expiry = field.get("value")
-          matched_dbs = True
-                                  
-      if matched_dbs is True:
-        member_record_dbs = {
-          "id": item.get("id"),
-          "first_name": item.get("first_name"),
-          "last_name": item.get("last_name"),
-          "email": item.get("email"),
-          "mobile": item.get("mobile"),
-          "dbs_expiry_date": dbs_expiry,
-        }
-        
-        members_dbs.append(member_record_dbs)
-
-      if matched_sg is True:
-        member_record_sg = {
-          "id": item.get("id"),
-          "first_name": item.get("first_name"),
-          "last_name": item.get("last_name"),
-          "email": item.get("email"),
-          "mobile": item.get("mobile"),
-          "safeguarding_expiry_date": safeguarding_expiry
-        }
-        
-        members_sg.append(member_record_sg)
   
+    # Break the while loop when no more records are returned
+    if not results:
+      logging.info(
+          "[retrieve_members_info] No more records found. Paginated API querying completes"
+      )
+      break
+    
+    # Add elements of results into members
+    members.extend(results)
+
+    logging.info(
+      "[retrieve_members_info] Retrieved %d records from page %d",
+      len(results),
+      page)
+    
+    # Move on next page
+    page += 1
+  
+  
+  # Generate two lists of members with DBS and SG information respectively
+  for member in members:
+    
+    # Place all key-value pairs of additional_field into a list
+    additional_fields = member.get("additional_fields", [])
+
+    # Initialised the matched flag
+    matched_dbs = False
+    matched_sg = False     
+    safeguarding_expiry = None
+    dbs_expiry = None
+    
+    # Loop through the additional fields of a member to check
+    # the existence of DBS or Safeguarding expiry information
+    for field in additional_fields:
+      if str(field.get("field_id")) == SG_FIELD_ID:
+        safeguarding_expiry = field.get("value")
+        matched_sg = True
+
+      if str(field.get("field_id")) == DBS_FIELD_ID:
+        dbs_expiry = field.get("value")
+        matched_dbs = True
+    
+    # Append the list members_dbs if DBS information is found
+    if matched_dbs is True:
+      member_dbs = {
+        "id": member.get("id"),
+        "first_name": member.get("first_name"),
+        "last_name": member.get("last_name"),
+        "email": member.get("email"),
+        "mobile": member.get("mobile"),
+        "dbs_expiry_date": dbs_expiry,
+      }
+      
+      members_dbs.append(member_dbs)
+
+    # Append the list members_sg if Safeguarding information is found
+    if matched_sg is True:
+      member_sg = {
+        "id": member.get("id"),
+        "first_name": member.get("first_name"),
+        "last_name": member.get("last_name"),
+        "email": member.get("email"),
+        "mobile": member.get("mobile"),
+        "safeguarding_expiry_date": safeguarding_expiry
+      }
+      
+      members_sg.append(member_sg)
+
   return members_dbs, members_sg
 
 
 def expiring_within_one_month(members: List[Dict], cert_type:str, days=30) -> list:
   
-  print(
+  """Filter the member list for DBS or safeguarding certificates that have expired
+  or are due to expire within one month from the time the script is executed.
+  Return the filtered list
+  """
+  
+  logging.info(
       "[expiring_within_one_month] Filtering the members with expiring " + cert_type + " certs"
   )
   
+  # Find out the cutoff date from today
   today = date.today()
   cutoff = today + timedelta(days=days)
 
-  # "YYYY-MM-DD" -> date
+  # Parse "YYYY-MM-DD" into a date object
   def parse_iso(d):
     try:
         return date.fromisoformat(d[:10]) if d else None
     except ValueError:
         return None
 
-  expiring = []
+  # Initialise the expiring list
+  expiring = list()
 
-  for m in members:
+  # Loop through the members and append those with expired or expiring DBS/Safeguarding certs
+  for member in members:
       if cert_type == "dbs":
-        dbs = parse_iso(m.get("dbs_expiry_date"))
+        dbs = parse_iso(member.get("dbs_expiry_date"))
         due = (dbs is not None) and (dbs <= cutoff)   # includes already expired
         
       if cert_type == "sg":
-        sg  = parse_iso(m.get("safeguarding_expiry_date"))
+        sg  = parse_iso(member.get("safeguarding_expiry_date"))
         due = (sg is not None) and (sg <= cutoff)   # includes already expired
 
       if due:
-          expiring.append(m)
+          expiring.append(member)
 
   return expiring
 
@@ -222,19 +284,15 @@ def expiring_to_excel_bytes(rows: List[Dict], sheet_name: str = "Expiring Certif
   Convert a list of dicts -> Excel (.xlsx) bytes in memory.
   """
   
-  print(
+  logging.info(
       "[expiring_to_excel_bytes] Converting the filtered list into Excel in memory"
   )
   
   if not rows:
-  
-    print(
-      "[expiring_to_csv_bytes] No list is found"
-    )
     logging.info(
-      "[expiring_to_csv_bytes] No list is found"
+      "[expiring_to_csv_bytes] No list is found."
     )
-    
+	  
     return b""
 
   wb = Workbook()
@@ -247,7 +305,7 @@ def expiring_to_excel_bytes(rows: List[Dict], sheet_name: str = "Expiring Certif
   for row in rows:
     ws.append([row.get(h) for h in headers])
 
-  # Optional: auto-size columns (nice-to-have)
+  # Optional: auto-size columns
   for col_idx, header in enumerate(headers, start=1):
     max_len = max(len(str(header)), *(len(str(r.get(header, ""))) for r in rows))
     ws.column_dimensions[get_column_letter(col_idx)].width = min(max_len + 2, 40)
@@ -259,8 +317,9 @@ def expiring_to_excel_bytes(rows: List[Dict], sheet_name: str = "Expiring Certif
 
 
 def gmail_service_from_refresh_token():
+  """Refreshing gmail service token"""
   
-  print(
+  logging.info(
       "[gmail_service_from_refresh_token] Refreshing gmail service token"
   )
   
@@ -284,6 +343,12 @@ def send_email_with_excel(
   body_html: str | None = None
   ):
   
+  """Sending gmail with Excel reports as attachment"""
+  
+  logging.info(
+      "[send_email_with_excel] Sending gmail with excel reports as attachment"
+  )
+  
   sender = GMAIL_SENDER
 
   msg = EmailMessage()
@@ -306,22 +371,42 @@ def send_email_with_excel(
     msg.add_attachment(file_bytes, maintype=maintype, subtype=subtype, filename=filename)
 
   raw = base64.urlsafe_b64encode(msg.as_bytes()).decode("utf-8")
+  
+  # Refresh the gmail API token
   service = gmail_service_from_refresh_token()
-  service.users().messages().send(userId="me", body={"raw": raw}).execute()
+  
+  # Send the email with verification of success
+  try:
+    response = service.users().messages().send(
+        userId="me",
+        body={"raw": raw}
+    ).execute()
 
+    message_id = response.get("id")
+
+    if message_id:
+        logging.info(
+            "[send_email_with_excel] Email sent successfully. Message ID: %s",
+            message_id,
+        )
+    else:
+        logging.warning(
+            "[send_email_with_excel] Email sent but no message ID returned"
+        )
+
+  except Exception as e:
+      logging.exception(
+          "[send_email_with_excel] Failed to send email"
+      )
+      raise
+  
 
 ##############################################
 ############## MAIN APPLICATION ##############
 ##############################################
 
 if __name__ == "__main__":
-  print("[main] In main")
-  
-  # Initialise the lists
-  member_info_dbs = list()
-  member_info_sg = list()
-  expiring_member_dbs = list()
-  expiring_member_sg = list()
+  logging.info("[main] Script running now...")
   
   # Retrieve members with DBS or Safeguarding cert
   member_info_dbs, member_info_sg = retrieve_members_info()
@@ -339,7 +424,7 @@ if __name__ == "__main__":
   excel_bytes_sg = expiring_to_excel_bytes(expiring_member_sg)
   
   # Place recipient address from the var which is delimited by comma
-  to_addrs = [x.strip() for x in EMAIL_TO.split(",") if x.strip()]
+  to_addrs = [x.strip() for x in GMAIL_TO.split(",") if x.strip()]
 
   # Send the email with Excel MIME type
   XLSX_MIME = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
